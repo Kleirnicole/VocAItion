0b715328d173ee3b2bd7cceb07ade5d827e3b45d
Kleir Nicole <kleirnicoleamaro@gmail.com>
2025-11-30 05:34:09 +0800

    Fix config.php path for Railway

--- php-app/index.php
+++ php-app/index.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 session_start();
 
 // Database connection
-require_once __DIR__ . '/php-app/db/config.php';
+require_once __DIR__ . '/db/config.php';
 
 try {
     echo "Connected to database successfully!";
