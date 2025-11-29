4c7c41ca65e88e7f82d50ceec541a50136c06ad9
Kleir Nicole <kleirnicoleamaro@gmail.com>
2025-11-30 04:31:45 +0800

    Update config.php to use Railway environment variables

--- php-app/db/config.php
+++ php-app/db/config.php
@@ -1,8 +1,10 @@
 <?php
-$host = 'localhost';
-$db   = 'vocaition_db';
-$user = 'root';
-$pass = '';
+$host = getenv('MYSQLHOST');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+
+define('PREDICT_API_URL', 'https://vocaiton-production.up.railway.app/predict');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
